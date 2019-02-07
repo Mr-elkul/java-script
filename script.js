5b526cc9e8363c3068c07a31f4a41a1091e11c69
Ahmed <mashaba@Ahmeds-MacBook-Pro.local>
2019-02-07 23:35:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+console.log('script');
+
+var num = 1;
+console.log(num);
+
+var num = num + 12;
+console.log(num);
+
+var num = num - 14;
+console.log(num);
+
+var num = num * 5;
+console.log(num);
+
+var num = num / 7;
+console.log(num);
+
+var num = num + 1;
+console.log(num);
+
+var num = num - 1;
+console.log(num);
+
+var num = num + 7;
+console.log(num);
+
+var num = num - 18;
+console.log(num);
+
+var num = num * 10;
+console.log(num);
+
+var num = num / 15;
+console.log(num);
+
+
+
+var hour = 12, minute = 30, second = 40;
+(hour == 12 && minute == 30 && second == 40), console.log('Time 12:30:40');
+
+
+var abcde = prompt('first stmbol is a?', '');
+
+if(abcde == 1) {
+     alert('yes');
+} else {
+    alert('no');
+}
+
+
+
+var str = "В некиим царстве, в некиим государстве жил-был богатый купец, именитый человек";
+str = 'богатый купец'
+console.log(str);
+
+
+var str = parseInt("3") + parseInt("5") + parseInt("3");
+console.log(str);
+
+var str = parseInt("3") == parseInt("5") == parseInt("3");
+console.log(str);
+
+
+var access;
+
+var a = prompt('is a equal to 10?', '');
+if (a == 10) {
+	access = true;
+	}else {
+		access = false
+}
+
+var isAdmin = confirm("Вы ‐ администратор?");
+alert( isAdmin );
+console.log(isAdmin);
+
+
+
+var a = ('test');
+access = (a = 3) ? true : false;
+console.log(a);
